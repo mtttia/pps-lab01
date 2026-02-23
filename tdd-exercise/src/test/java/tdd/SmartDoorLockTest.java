b3a661ec5aa2e4fb2b6742d0009fa12abf0af963
mtttia <mattia.senni.03@gmail.com>
2026-02-23 14:00:06 +0100

    feature: cannot lock a smart door lock without a setting a pin

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,20 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+    private SmartDoorLock smartDoorLock;
+
+    @BeforeEach
+    void beforeEachInitializeSmartDoorLock(){
+        smartDoorLock = new SmartDoorLockImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void testLockWithoutPin(){
+        assertThrows(IllegalStateException.class, () -> smartDoorLock.lock());
     }
 }
