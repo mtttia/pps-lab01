ab2f26939680b9744cd2e53715c413cb49b2b8a4
mtttia <mattia.senni.03@gmail.com>
2026-02-23 15:26:46 +0100

    feature: cannot pop on empty stack

---
+++ tdd-exercise/src/main/java/tdd/MinMaxStackImpl.java
@@ -0,0 +1,38 @@
+package tdd;
+
+public class MinMaxStackImpl implements MinMaxStack {
+    @Override
+    public void push(int value) {
+
+    }
+
+    @Override
+    public int pop() {
+        throw new IllegalStateException("Stack is empty");
+    }
+
+    @Override
+    public int peek() {
+        return 0;
+    }
+
+    @Override
+    public int getMin() {
+        return 0;
+    }
+
+    @Override
+    public int getMax() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -1,12 +1,20 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+    private MinMaxStack minMaxStack;
+
+    @BeforeEach
+    void beforeEach(){
+        minMaxStack = new MinMaxStackImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void testCannotPopOnEmptyStack(){
+        assertThrows(IllegalStateException.class, () -> minMaxStack.pop());
     }
 }
